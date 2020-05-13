4d558e91256b7c1879e4fd9fbfb5ba6311dff714
Sayan <sayan227sil@ymail.com>
2020-05-13 22:52:16 +0530

    Add more unittests

--- pandu-organiser-app/app/src/androidTest/java/team/OG/pandu_organiser/InstrumentedUnitTests.java
+++ pandu-organiser-app/app/src/androidTest/java/team/OG/pandu_organiser/InstrumentedUnitTests.java
@@ -2,15 +2,20 @@ package team.OG.pandu_organiser;
 
 
 import android.content.Context;
+
+import androidx.annotation.NonNull;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import team.OG.pandu_organiser.Units.Pandal;
 
 import static org.junit.Assert.*;
 
@@ -20,10 +25,12 @@ public class InstrumentedUnitTests {
 
     private boolean[] ctrl;
     private int actual_db_size;
+    private String test_theme;
 
     @Before
     public void init() {
-        int n_test = 3;
+        int n_test = 4;
+        test_theme = "test456";
         actual_db_size = 6;
         ctrl = new boolean[n_test];
     }
@@ -74,6 +81,46 @@ public class InstrumentedUnitTests {
             }
             ctrl[test_number - 1] = true;
         });
+
+        while (!ctrl[test_number - 1]);
+
+    }
+
+    @Test // # 4
+    public void updatePandelInfo() {
+        final int test_number = 4;
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("pandels").document("4VCrJukancYOegKkncB409HhE363")
+                .set(new Pandal(
+                        "Test Pandal",
+                        100,
+                        "",
+                        test_theme,
+                        "https://201758-624029-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2017/03/img-placeholder.png"
+                ))
+                .addOnSuccessListener((Void aVoid) -> {
+
+                    db.collection("pandels").document("4VCrJukancYOegKkncB409HhE363")
+                            .get()
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    DocumentSnapshot document = task.getResult();
+                                    if (document.exists()) {
+                                        assertEquals(test_theme, document.getString("theme"));
+                                    } else {
+                                        fail();
+                                    }
+                                }
+
+                                ctrl[test_number - 1] = true;
+                            });
+
+                })
+                .addOnFailureListener((@NonNull Exception e) -> {
+                    fail();
+                    ctrl[test_number - 1] = true;
+                });
 
         while (!ctrl[test_number - 1]);
 
